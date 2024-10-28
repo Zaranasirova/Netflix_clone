0612ffbae49edd81cc659ee894d1594947ff72eb
Zaranasirova <zerifenesirova784@gmail.com>
2024-10-28 13:13:57 +0400

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <header>
+    <div className="container">
+        <div className="left-side">
+
+        </div>
+        <div className="right-side"></div>
+    </div>
+   </header>
+  )
+}
+
+export default Header
