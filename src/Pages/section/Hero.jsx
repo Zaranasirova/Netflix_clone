e99a017dbf7c9ee8b576d759ffaeba2382c1f4f0
Zaranasirova <zerifenesirova784@gmail.com>
2024-10-28 18:00:29 +0400

    Complete Hero Section SCSS

--- src/Pages/section/Hero.jsx
+++ src/Pages/section/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import bg_image from "../../assets/image/hero_banner.jpg";
 import hero_title from "../../assets/image/hero_title.png";
+import play_button from "../../assets/icons/play_icon.png";
+import info_icon from  "../../assets/icons/info_icon.png"
 
 const Hero = () => {
     return (
@@ -8,10 +10,14 @@ const Hero = () => {
             <div className="hero">
                 <img src={bg_image} alt="hero-bg-image" className='banner-image' />
                 <div className="container">
-                <div className="hero-caption">
-                    <img src={hero_title} alt="hero-title-image" className='caption-img' />
-                    <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
-                </div>
+                    <div className="hero-caption">
+                        <img src={hero_title} alt="hero-title-image" className='caption-img' />
+                        <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                        <div className="hero-btns">
+                            <button className='btn'><img src={play_button} alt="play-icon"/>Play</button>
+                            <button className='btn dark-btn'><img src={info_icon} alt="info-icon" />More İnfo</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
