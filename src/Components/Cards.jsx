b56724a05a206f8db5d1e9fd0ff60563a8b4a5e9
Zaranasirova <zerifenesirova784@gmail.com>
2024-10-29 15:02:32 +0400

    card-slide

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -10,13 +10,21 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css/pagination';
 
 const Cards = () => {
+    const getSlidesPerView = () => {
+        const width = window.innerWidth;
+        if (width >= 1200) return 6;
+        if (width >= 992) return 5; 
+        if (width >= 768) return 4; 
+        if (width >= 576) return 3; 
+        return 1; 
+    };
     return (
         <div className="title-cards">
         <h2>Popular on Netflix</h2>
         <Swiper
-               slidesPerView={4}
+               slidesPerView={getSlidesPerView()}
                centeredSlides={false}
-               spaceBetween={0} // Boşluğu 0 olaraq təyin edin
+               spaceBetween={110} 
                grabCursor={true}
                pagination={{
                  clickable: true,
@@ -25,7 +33,7 @@ const Cards = () => {
                className="mySwiper"
         >
             {card_data.map((item, index) => (
-                <SwiperSlide key={index}>
+                <SwiperSlide key={index} className='card-list'>
                     <div className="card-image">
                         <img src={item.image} alt={item.name} />
                     </div>
